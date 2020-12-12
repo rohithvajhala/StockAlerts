577f27e1e7264410ad0cb193dabb4d82ff2b0a82
Rohith Vajhala <rohithvajhala@gmail.com>
2020-12-12 17:38:43 +0100

    Show some popular stocks and add search functionality

--- stock_alerts/pages/urls.py
+++ stock_alerts/pages/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home_view, name="home_page"),
-    path('home/', views.home_view, name='home_page')]
+    path('home/', views.home_view, name='home_page'),
+    path('update_quotes/', views.update_quotes, name='update_quotes'), ]
--- stock_alerts/pages/views.py
+++ stock_alerts/pages/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from api.stocks import get_popular_stocks, search_stocks
 
 
 # Create your views here.
+def update_quotes(request):
+    contents = {
+        'stocks': get_popular_stocks(),
+    }
+
+    return render(request, 'update_quotes.html', contents)
+
+
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    contents = {}
+    if request.POST:
+        query = request.POST.get('query')
+        stocks = search_stocks(query)
+        contents = {
+            'stocks': stocks,
+        }
+        return render(request, "search.html", contents)
+
+    contents = {
+        'stocks': get_popular_stocks(),
+    }
+
+    return render(request, "home.html", contents)
