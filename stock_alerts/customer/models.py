8d019b309c4454969153e3f314f03b053c0cd771
Rohith Vajhala <rohithvajhala@gmail.com>
2020-12-13 14:42:25 +0100

    Create customer model

--- stock_alerts/customer/models.py
+++ stock_alerts/customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True,
+                                on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=128, blank=True)
+    email = models.EmailField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
