7a3618453d66585fcfa030b775272f8c5e174844
Rohith Vajhala <rohithvajhala@gmail.com>
2020-12-13 15:28:06 +0100

    Add login logout functionality

--- stock_alerts/customer/urls.py
+++ stock_alerts/customer/urls.py
@@ -4,4 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('sign_up/', views.sign_up_view, name='sign_up'),
+    path('login/', views.login_view, name='login_page'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- stock_alerts/customer/views.py
+++ stock_alerts/customer/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from customer.forms import Create_user_form
 from django.contrib import messages
 from .decorators import unauthenticated_user
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -18,9 +19,29 @@ def sign_up_view(request):
             user = form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, 'Account created successfully for ' + username)
-            return redirect('home_page')
+            return redirect('login_page')
         else:
             print("form verification failed")
 
     context = {'form': form}
     return render(request, "sign_up.html", context)
+
+@unauthenticated_user
+def login_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        username = request.POST.get('userName')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username,
+                            password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home_page')
+        else:
+            messages.info(request, 'Incorrect Username or Password!')
+
+    return render(request, "login_page.html", {})
+
+
+def logout_view(request, *args, **kwargs):
+    logout(request)
+    return redirect('login_page')
